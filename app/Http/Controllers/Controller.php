dd74f7048c19db335cf4717b614536486053f1a4
Dhika Ramdani <103547470+Dhikaramdani310@users.noreply.github.com>
2025-07-12 18:56:37 +0700

    Update Controller.php

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -2,6 +2,28 @@
 
 namespace App\Http\Controllers;
 
+/**
+ * @OA\Info(
+ * version="1.0.0",
+ * title="UTS PWEB API Documentation",
+ * description="API Documentation for Ujian Tengah Semester Pemrograman Web Lanjut",
+ * @OA\Contact(
+ * email="admin@example.com"
+ * )
+ * )
+ *
+ * @OA\Server(
+ * url=L5_SWAGGER_CONST_HOST,
+ * description="Demo API Server"
+ * )
+ *
+ * @OA\SecurityScheme(
+ * securityScheme="sanctum",
+ * type="http",
+ * scheme="bearer",
+ * bearerFormat="JWT"
+ * )
+ */
 abstract class Controller
 {
     //
